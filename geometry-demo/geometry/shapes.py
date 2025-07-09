12a5a0ca1015ac42bb1d8ee683ddbeee7c2b8f08
ruirise <ruihuang@bu.edu>
2025-07-08 21:36:49 -0400

    move pycache

--- geometry-demo/geometry/shapes.py
+++ geometry-demo/geometry/shapes.py
@@ -1,17 +1,17 @@
 from abc import ABC, abstractmethod
 import math
 class Shape(ABC): 
-    def area(self):
+    def area(self) -> float:
         pass
 class Square(Shape):
     def __init__(self,side):
         self.side = side
-    def area(self):
+    def area(self) -> float:
         return self.side **2
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-    def area(self):
+    def area(self) -> float:
         return math.pi* self.radius **2
     
 
